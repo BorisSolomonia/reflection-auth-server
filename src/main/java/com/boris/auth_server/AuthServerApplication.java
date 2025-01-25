7fcdea4e923d7f3e3f6982ea07617691d265bda3
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2025-01-25 19:07:52 +0400

    discover

--- src/main/java/com/boris/auth_server/AuthServerApplication.java
+++ src/main/java/com/boris/auth_server/AuthServerApplication.java
@@ -3,12 +3,10 @@ package com.boris.auth_server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
